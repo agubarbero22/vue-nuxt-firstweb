3470ec5ec5c68477c5d29ec1aaeafbd89e7006d6
agubarbero22 <agubarbero22@gmail.com>
2022-02-22 20:08:09 -0300

    First commit

---
+++ components/ToDoItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="checkbox" id="todo-item" v-model="toggle"/>
+        <label for="todo-item">My todo Item</label>
+    </div>
+</template>
+
+
+<script>
+  export default {
+
+  };
+</script>
---
+++ pages/fun.vue
@@ -0,0 +1,3 @@
+<template>
+    <v-alert @click="show(false)"> HOLA </v-alert>
+</template>
